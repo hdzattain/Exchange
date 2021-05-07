c7ffbbb4dc561c68d4b235f31de879d8b4621db5
hdzattain <58393005+hdzattain@users.noreply.github.com>
2021-05-07 21:38:35 +0800

    Create functor.cpp
    generic parameter to declare two different sets.

---
+++ functor.cpp
@@ -0,0 +1,15 @@
+struct BuyOrderCompare {
+  bool operator() (int i, int j) {
+    const Order& oi = orders[i];
+    const Order& oj = orders[j];
+    return oi.price > oj.price || (oi.price == oj.price && i < j);
+  }
+};
+struct SellOrderCompare {
+  bool operator() (int i, int j) {
+    const Order& oi = orders[i];
+    const Order& oj = orders[j];
+    return oi.price < oj.price || (oi.price == oj.price && i < j);
+  }
+};
+
